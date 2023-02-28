7df6bf6087def7354e441ec28139e32de0c2051d
minharaza <razamh@mail.uc.edu>
2023-02-28 11:46:10 -0500

    starting on contact page

--- BearcatHelper.UI/src/app/app.module.ts
+++ BearcatHelper.UI/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, ContactPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
