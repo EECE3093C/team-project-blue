ab96b1ab3f185261e30d97777327f4393aa8a13c
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2023-02-28 13:45:51 -0500

    adding the login system (#29)

--- BearcatHelper.UI/src/app/app-routing.module.ts
+++ BearcatHelper.UI/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { UserAuthContainerComponent } from './user-auth-container/user-auth-container.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+    title: 'BearcatHelper',
+  },
+  {
+    path: 'login',
+    component: UserAuthContainerComponent,
+    title: 'Login | BearcatHelper',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BearcatHelper.UI/src/app/app.module.ts
+++ BearcatHelper.UI/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { UserAuthContainerComponent } from './user-auth-container/user-auth-container.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, LoginFormComponent, SignupFormComponent, UserAuthContainerComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
