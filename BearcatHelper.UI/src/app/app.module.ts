a06c326b09c23b1ce4df76fa87a315bea9fa91de
Prabhjot Singh <97115364+PrabhjotxSingh@users.noreply.github.com>
2023-03-02 01:15:08 -0500

    signup-ui (#31)

--- BearcatHelper.UI/src/app/app.module.ts
+++ BearcatHelper.UI/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,10 +8,12 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { UserAuthContainerComponent } from './user-auth-container/user-auth-container.component';
+import { ErrorNotFoundComponent } from './error-not-found/error-not-found.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent, LoginFormComponent, SignupFormComponent, UserAuthContainerComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomepageComponent, LoginFormComponent, SignupFormComponent, UserAuthContainerComponent, ErrorNotFoundComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule, MatIconModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
