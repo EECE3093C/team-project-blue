6a29b74263fd2e9f5cdb7fc21850c95d4ae5b9d5
minharaza <razamh@mail.uc.edu>
2023-03-02 14:52:04 -0500

    getting local host working

--- BearcatHelper.UI/src/app/app-routing.module.ts
+++ BearcatHelper.UI/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [{ path: '', component: HomepageComponent }, {path: 'contact', component: ContactPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
