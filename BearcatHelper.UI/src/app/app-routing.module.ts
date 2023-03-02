c50586092c9f9d646ea48d1e3dc7aebe5adb5e4f
Hannah Beals <bealshm@mail.uc.edu>
2023-03-02 13:42:41 -0500

    adding about-page routing and beginning structure

--- BearcatHelper.UI/src/app/app-routing.module.ts
+++ BearcatHelper.UI/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
 import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [{ path: '', component: HomepageComponent }];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'about-page', component: AboutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- BearcatHelper.UI/src/app/app.module.ts
+++ BearcatHelper.UI/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, AboutPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
